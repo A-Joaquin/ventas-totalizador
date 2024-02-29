7467f554425617b30819e6c2c0de750bd6a9dcc4
arturo <arturo.veliz@ucb.edu.bo>
2024-02-29 09:47:37 -0400

    calcular precio con impuestos de CA

--- src/calcular.js
+++ src/calcular.js
@@ -1,6 +1,17 @@
-function calcularprecio(cantidad,precio) {
-    return cantidad*precio;
+function calcularprecio(cantidad,precio,estado) {
+  let precioNeto;
+  let tazaImpuestos=1;
+  precioNeto=cantidad*precio;
+  if(estado==="CA")
+  {
+    tazaImpuestos=0.0825;
+    precioNeto=precioNeto+(precioNeto*tazaImpuestos);
   }
+
+  return precioNeto;
+}
   
   export default calcularprecio;
+
+
   
--- src/calcular.test.js
+++ src/calcular.test.js
@@ -2,6 +2,13 @@ import calcularprecio from "./calcular.js";
 
 describe("Calcular precios", () => {
   it("Deberia darme el precio neto", () => {
-    expect(calcularprecio(3, 2)).toEqual(6);
+    expect(calcularprecio(3, 2,"")).toEqual(6);
+  });
+
+});
+
+describe("Calcular precios con impuestos", () => {
+  it("Deberia darme el precio neto con impuesto de CA", () => {
+    expect(calcularprecio(5, 2,"CA")).toEqual(10+0.825);
   });
 });
